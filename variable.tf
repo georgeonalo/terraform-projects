d15e7cce5dfda351e6fe150c4ea89e52c5630b52
George Onalo <Onalotech7@gmail.com>
2023-03-21 08:52:56 +0100

    created the security group file

--- variable.tf
+++ variable.tf
@@ -40,3 +40,10 @@ variable "private_data_subnet_az2_cidr" {
     description = "private data subnet az2 cidr block"
     type        = string 
 }
+
+# security group variables
+variable "ssh_location" {
+    default     = "0.0.0.0/0"
+    description = "the ip address that can ssh into the ec2 intances"
+    type        = string 
+}
